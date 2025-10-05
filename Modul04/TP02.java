c9b751653fd1e1755b6de993325f690e6de3d998
Diva Ananda <dvaannda30@gmail.com>
2025-10-05 20:26:47 +0700

    mengumpulkan Modul04

--- Modul04/TP02.java
+++ Modul04/TP02.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class TP02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        double diskon = 0;
+        if (N > 200000) {
+            diskon = N * 0.05; // 5%
+        }
+
+        double total = N - diskon;
+
+        double ppn = total * 0.11;
+
+        double totalBayar = total + ppn;
+
+        System.out.println("Transaksi = " + N);
+        if (diskon > 0) {
+            System.out.println("Total =" + N + " - " + (int)diskon);
+        } else {
+            System.out.println("Total = " + N);
+        }
+        System.out.println("Ppn 11% = " + ppn);
+        System.out.println("Total dibayar = " + totalBayar);
     }
 }
--- Modul04/TP03.java
+++ Modul04/TP03.java
@@ -1,6 +1,19 @@
-public class TP03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int D = input.nextInt();
+        int R = input.nextInt();
+        int I = input.nextInt();
+
+        if (D > R && D > I) {
+            System.out.println("Dira");
+        } else if (R > D && R > I) {
+            System.out.println("Radi");
+        } else {
+            System.out.println("Idar");
+        }
     }
 }
