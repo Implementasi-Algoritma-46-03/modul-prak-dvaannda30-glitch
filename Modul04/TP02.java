d7aa93162ffb28cd7540fbd09ed63287a0f0408d
Diva Ananda <dvaannda30@gmail.com>
2025-10-05 21:23:09 +0700

    Menambahkan TP Modul04

--- Modul04/TP02.java
+++ Modul04/TP02.java
@@ -4,20 +4,27 @@ public class TP02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // input total transaksi
         int N = input.nextInt();
 
+        // hitung diskon (jika ada)
         double diskon = 0;
-        if (N > 200000) {
+        if (N > 2 00000) {
             diskon = N * 0.05; // 5%
         }
 
+        // total setelah diskon
         double total = N - diskon;
+
+        // hitung PPN 11% dari total
         double ppn = total * 0.11;
+
+        // total akhir dibayar
         double totalBayar = total + ppn;
-        
+
         System.out.println("Transaksi = " + N);
         if (diskon > 0) {
-            System.out.println("Total =" + N + " - " + (int)diskon);
+            System.out.println("Total = " + N + " - " + (int)diskon);
         } else {
             System.out.println("Total = " + N);
         }
