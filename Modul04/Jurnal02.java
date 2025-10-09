d50be01762767ea15d558a156e5e2a80e8e83063
Diva Ananda <dvaannda30@gmail.com>
2025-10-09 19:00:55 +0700

    memperbaiki jurnal

--- Modul04/Jurnal02.java
+++ Modul04/Jurnal02.java
@@ -2,41 +2,30 @@ import java.util.Scanner;
 public class Jurnal02 {
 
     public static void main(final String[] args) {
+
+        // Kerjakan soalnya di sini
+
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-        input.close();
+        double pajakPersen;
 
-        if (N > 0 &&  N <= 50) {
-            System.out.println("Penghasilan kotor = " + N + "juta Rupiah");
-            N *= 1000000;
-            int pajak = N * 5/100;
-            int hasilBersih = N - pajak;
-            System.out.println("Pajak 5% = Rp. " + pajak);
-            System.out.println("Penghasilan bersih = Rp. " + hasilBersih);
-
-        }else if (N >= 51 && N <= 250) {
-            System.out.println("Penghasilan kotor = " + N + "juta Rupiah");
-            N *= 1000000;
-            int pajak = N * 15/100;
-            int hasilBersih = N - pajak;
-            System.out.println("Pajak 15% = Rp. " + pajak);
-            System.out.println("Penghasilan bersih = Rp. " +  hasilBersih); 
-
-        }else if (N >= 251 && N <= 500) {
-            System.out.println("Penghasilan kotor = " + N + "juta Rupiah");
-            N *= 1000000;
-            int pajak = N * 25/100;
-            int hasilBersih = N - pajak;
-            System.out.println("Pajak 25% = Rp. " + pajak);
-            System.out.println("Penghasilan bersih = Rp. " + hasilBersih);
-
-        }else{
-            System.out.println("Penghasilan kotor = " + N + "juta Rupiah");
-            N *= 1000000;
-            long pajak = (long) N * 30/100;
-            long hasilBersih = (long) N - pajak;
-            System.out.println("Pajak 30% = Rp. " + pajak);
-            System.out.println("Penghasilan bersih = Rp. " +  hasilBersih); 
+        if (N <= 50) {
+            pajakPersen = 5;
+        } else if (N <= 250) {
+            pajakPersen = 15;
+        } else if (N <= 500) {
+            pajakPersen = 25;
+        } else {
+            pajakPersen = 30;
         }
+
+        double besarPajak = N * 1_000_000 * (pajakPersen / 100);
+        double penghasilanBersih = (N * 1_000_000) - besarPajak;
+
+        System.out.println("Penghasilan kotor = " + N + " juta Rupiah");
+        System.out.println("Pajak " + (int)pajakPersen + "% = Rp. " + (int)besarPajak);
+        System.out.println("Penghasilan bersih = Rp. " + (int)penghasilanBersih);
+
+        input.close();
     }
 }
--- Modul04/Jurnal03.java
+++ Modul04/Jurnal03.java
@@ -1,6 +1,32 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
-    }
-}
+public class Jurnal03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Membaca input nilai Dira (D), Radi (R), dan Idar (I)
+        int dira = input.nextInt();
+        int radi = input.nextInt();
+        int idar = input.nextInt();
+
+        // 6 kondisi untuk menentukan urutan nilai dari terkecil ke terbesar
+        if (dira < radi && radi < idar) {
+            System.out.println("Dira, Radi, Idar");
+        } 
+        else if (dira < idar && idar < radi) {
+            System.out.println("Dira, Idar, Radi");
+        } 
+        else if (radi < dira && dira < idar) {
+            System.out.println("Radi, Dira, Idar");
+        } 
+        else if (radi < idar && idar < dira) {
+            System.out.println("Radi, Idar, Dira");
+        } 
+        else if (idar < dira && dira < radi) {
+            System.out.println("Idar, Dira, Radi");
+        } 
+        else if (idar < radi && radi < dira) {
+            System.out.println("Idar, Radi, Dira");
+        }
+
+    }}    
