418a8df4828532d82c2fe2d84d75389f46882063
Diva Ananda <dvaannda30@gmail.com>
2025-10-16 19:36:14 +0700

    update

--- Modul03/TP03.java
+++ Modul03/TP03.java
@@ -1,42 +1,22 @@
 import java.util.Scanner;
-
-public class TP03 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int jumlahPermen = input.nextInt();
-        int jumlahTeman = input.nextInt();
-        int totalOrang = jumlahTeman + 1;
-        int jatahPerOrang = jumlahPermen / totalOrang;
-        int sisaPermen = jumlahPermen % totalOrang;
-        System.out.println(jatahPerOrang);
-        System.out.println(sisaPermen);
-        input.close();
-    }
-}
-
-/**PS E:\Pasyaaaaaaa\d3if-49-03-Pasya16> cd Modul01
-PS E:\Pasyaaaaaaa\d3if-49-03-Pasya16\Modul01> javac TP03.java
-E:\Pasyaaaaaaa\d3if-49-03-Pasya16\Modul01> java TP03
-12 3
-3
-0*/
 public class TP03 {
 
     public static void main(final String[] args) {
-        // TP03
-        Scanner tugas3 = new Scanner(System.in);
+        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
 
-        int jumlahpermen = tugas3.nextInt();
-        int jumlahteman = tugas3.nextInt();
+        int n = input.nextInt();
+        int t = input.nextInt();
 
-        int total = jumlahteman + 1;
+        int totalOrang = t + 1;
 
-        int orang = jumlahpermen / total;
-        int permen = jumlahpermen % orang;
+        int jatah = n / totalOrang;
+        int sisa = n % totalOrang;
 
-        System.out.println(orang);
-        System.out.println(permen);
+        // Output       
+        System.out.println(jatah);
+        System.out.println(sisa);
 
-        tugas3.close();
+        input.close();
     }
 }
