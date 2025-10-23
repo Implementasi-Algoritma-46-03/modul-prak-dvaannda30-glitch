6bd7904b5b7fc937cbcdabf085e44625e642ab05
Diva Ananda <dvaannda30@gmail.com>
2025-10-23 18:11:08 +0700

    kjnbjk

--- Modul06/Jurnal01.java
+++ Modul06/Jurnal01.java
@@ -2,6 +2,30 @@ import java.util.Scanner;
 public class Jurnal01 {
 
     public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nama1 = input.nextLine();
+        String nama2 = input.nextLine();
         
+        int sapaan = input.nextInt();
+
+        greeting(nama1, sapaan);
+        greeting(nama2, sapaan);
+
+        input.close();
+        // System.out.println("Hallo, " + nama1 + ". Selamat " + sapaan(jam) + ".");
+        // System.out.println("Hallo, " + nama2 + ". Selamat " + sapaan(jam) ".");
     }
+    private static void greeting(String nama, int waktu){
+        System.out.print("Hallo, " + nama);
+        if (waktu >=  6 && waktu <= 11){
+            System.out.println(". Selamat pagi.");
+        }else if (waktu >= 12 && waktu <= 14){
+            System.out.println(". Selamat siang.");
+        }else if (waktu >= 15 && waktu <= 17){
+            System.out.println(". Selamat sore.");
+        }else if (waktu >= 18 && waktu <= 5){
+            System.out.println(". Selamat malam.");
+        }
+        }
+
 }
--- Modul06/Jurnal02.java
+++ Modul06/Jurnal02.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
 public class Jurnal02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+        String lagu = input.nextLine();
+        lirik(lagu);
+    }
+    
+
+    private static void lirik(String lagu) {
+        String lirik = lagu;
+        int teks = lirik.length();
+        switch (teks % 5) {
+            case 0:
+                lirik = lirik.replaceAll("[AIUEOaiueo]", "a");
+                break;
+            case 1:
+                lirik = lirik.replaceAll("[AIUEOaiueo]", "e");
+                break;
+            case 2:
+                lirik = lirik.replaceAll("[AIUEOaiueo]", "i");
+                break;
+            case 3:
+                lirik = lirik.replaceAll("[AIUEOaiueo]", "o");
+                break;
+            case 4:
+                lirik = lirik.replaceAll("[AIUEOaiueo]", "u");
+                break;
+        }
+        System.out.println(lirik);
     }
 }
